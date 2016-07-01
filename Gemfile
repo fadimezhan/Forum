89e727e9cae888a0afcfeaaf054d30a2cab55797
Fadime Özhan <fadime@ismails-iMac.local>
2016-07-01 11:49:40 +0300

    Added User Devise & Associate With Post

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml', '~> 4.0.7'
 gem 'simple_form', '~> 3.2.1'
+gem 'devise', '~> 4.1.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,18 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show,:edit,:update,:destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
   end
 
   def show
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.post.build(post_params)
     if @post.save
       redirect_to @post
     else
